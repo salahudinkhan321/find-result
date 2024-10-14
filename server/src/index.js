197bb4c7cc9087b6de0f0b353a0d5fb048c697d9
Salahudin Saani <udinsalah003@gmail.com>
2024-10-14 16:33:44 +0500

    cors

--- server/src/index.js
+++ server/src/index.js
@@ -2,15 +2,19 @@ import express from 'express'
 import { connectDb } from './db/index.js'
 import dotenv from 'dotenv'
 import studentRouter from './routes/student.route.js'
+import cors from 'cors'
 
 dotenv.config({ path: './.env' })
+
 const app = express()
 const port = process.env.PORT || 4000
 
 
 app.use(express.json())
 app.use('/api/student', studentRouter)
-
+app.use(cors({
+    origin: process.env.FRONTEND_URI
+}))
 
 connectDb()
     .then(
